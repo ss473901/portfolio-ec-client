3e08f45e5c7b9266060cfb18ec76773949686a8d
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-03-14 20:18:01 +0900

    Add Cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,3 +1,15 @@
+import * as Styled from "./style";
+import { Navbar } from "../../component/navbar/Navbar";
+
 export const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <Styled.Container>
+      <Navbar />
+      <Styled.Wrapper>
+        <Styled.Title>YOUR BAG</Styled.Title>
+        <Styled.Top></Styled.Top>
+        <Styled.Bottom></Styled.Bottom>
+      </Styled.Wrapper>
+    </Styled.Container>
+  );
 };
---
+++ src/pages/cart/style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+export const Wrapper = styled.div`
+  padding: 20px;
+
+  border: 1px red solid;
+`;
+export const Title = styled.h1``;
+export const Top = styled.div``;
+export const Bottom = styled.div``;
