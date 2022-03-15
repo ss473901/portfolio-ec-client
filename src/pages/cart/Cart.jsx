39aadb5cc601390e706b423e71421e161ca85049
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-03-15 12:35:35 +0900

    add Cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -5,10 +5,54 @@ export const Cart = () => {
   return (
     <Styled.Container>
       <Navbar />
+
       <Styled.Wrapper>
-        <Styled.Title>YOUR BAG</Styled.Title>
-        <Styled.Top></Styled.Top>
-        <Styled.Bottom></Styled.Bottom>
+        <Styled.Title>CART</Styled.Title>
+
+        <Styled.Top>
+          <Styled.TopButton>買い物を続ける</Styled.TopButton>
+          <Styled.TopButton type="filled">決済をする</Styled.TopButton>
+        </Styled.Top>
+
+        <Styled.Bottom>
+          <Styled.Info>
+            <Styled.Product>
+              <Styled.ProductDetail>
+                <Styled.Image />
+
+                <Styled.Details>
+                  <Styled.ProductName>
+                    <b>商品名：</b>シャツ
+                  </Styled.ProductName>
+                  <Styled.ProductId>
+                    <b>ID：</b>888888
+                  </Styled.ProductId>
+                  <Styled.ProductColor />
+                  <Styled.ProductSize>
+                    <b>サイズ：</b>M
+                  </Styled.ProductSize>
+                </Styled.Details>
+              </Styled.ProductDetail>
+            </Styled.Product>
+            <Styled.PriceDetail>Price</Styled.PriceDetail>
+          </Styled.Info>
+          <Styled.Summary>
+            <Styled.SummaryTitle>商品合計</Styled.SummaryTitle>
+            <Styled.SummaryItem>
+              <Styled.SummaryItemText>シャツ</Styled.SummaryItemText>
+              <Styled.SummaryItemPrice>1500円</Styled.SummaryItemPrice>
+            </Styled.SummaryItem>
+            <Styled.SummaryItem>
+              <Styled.SummaryItemText>シャツ</Styled.SummaryItemText>
+              <Styled.SummaryItemPrice>1500円</Styled.SummaryItemPrice>
+            </Styled.SummaryItem>
+            <Styled.SummaryItem>
+              <Styled.SummaryItemText>Total</Styled.SummaryItemText>
+              <Styled.SummaryItemPrice>1500円</Styled.SummaryItemPrice>
+            </Styled.SummaryItem>
+            <Styled.Button>決済する</Styled.Button>
+          </Styled.Summary>
+        </Styled.Bottom>
       </Styled.Wrapper>
     </Styled.Container>
   );
--- src/pages/cart/style.jsx
+++ src/pages/cart/style.jsx
@@ -1,11 +1,51 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
 export const Wrapper = styled.div`
   padding: 20px;
-
-  border: 1px red solid;
 `;
-export const Title = styled.h1``;
-export const Top = styled.div``;
-export const Bottom = styled.div``;
+
+export const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+export const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Info = styled.div`
+  flex: 3;
+`;
+
+export const Product = styled.div``;
+export const ProductDetail = styled.div``;
+export const Image = styled.img``;
+export const PriceDetail = styled.div``;
+export const Details = styled.div``;
+export const ProductName = styled.span``;
+export const ProductId = styled.span``;
+export const ProductColor = styled.div``;
+export const ProductSize = styled.span``;
+
+export const Summary = styled.div`
+  flex: 1;
+`;
+export const SummaryTitle = styled.div``;
+export const SummaryItem = styled.div``;
+export const SummaryItemText = styled.span``;
+export const SummaryItemPrice = styled.span``;
+export const Button = styled.button``;
